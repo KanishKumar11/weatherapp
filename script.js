9177b8d74c97ac1e25abb1cc8284c8787ada9aed
Kanish Kumar <keenkanish@gmail.com>
2021-10-25 21:41:37 -0700

    mobile error sovled in css

--- script.js
+++ script.js
@@ -7,8 +7,23 @@ const iconElement = document.querySelector(".weather-icon");
 const notificationElement = document.querySelector(".notification");
 const wIcon = document.getElementById("w_icon");
 
-// location Detecting]
+// location Detecting
+if (navigator.geolocation) { //check if geolocation is available
+    navigator.geolocation.getCurrentPosition(function(position){
+      console.log(position);
+      let pos1 = position.coords.latitude;
+      let pos2 = position.coords.longitude;
+      const api = "https://api.openweathermap.org/data/2.5/weather?lat="+pos1+"&lon="+pos2+"&units=metric&appid=1bffc22256ef642e8130ab1a1ff621ee";
+      fetch(api)
+      .then(response => response.json())
+      .then(data => {
+          let cityVal = data['name'];
 
+          cityName.innerHTML = cityVal;
+      });
+      console.log(api);
+    })
+}     
 // Data Fetching
 btn.addEventListener('click',function(){
     fetch('https://api.openweathermap.org/data/2.5/weather?q='+input.value+'&units=metric&appid=1bffc22256ef642e8130ab1a1ff621ee')
