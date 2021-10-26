76e524646b289151bf7478263ffb988585509775
Kanish Kumar <keenkanish@gmail.com>
2021-10-25 23:17:50 -0700

    location detection feature added

--- script.js
+++ script.js
@@ -13,14 +13,21 @@ if (navigator.geolocation) { //check if geolocation is available
       console.log(position);
       let pos1 = position.coords.latitude;
       let pos2 = position.coords.longitude;
+
+      
       const api = "https://api.openweathermap.org/data/2.5/weather?lat="+pos1+"&lon="+pos2+"&units=metric&appid=1bffc22256ef642e8130ab1a1ff621ee";
       fetch(api)
       .then(response => response.json())
       .then(data => {
-          let cityVal = data['name'];
-
-          
-          cityName.innerHTML = cityVal;
+        let cityVal = data['name']+","+data['sys']['country'];
+        let tempVal = data['main']['temp'];
+        let descVal = data['weather'][0]['description'];
+        let iconVal = data['weather'][0]['icon'];
+    
+        cityName.innerHTML = cityVal;
+        temp.innerHTML = tempVal+"°"+"<span>C</span>";
+        desc.innerHTML = descVal;
+        wIcon.src = "./resources/icons/"+iconVal+".png";
       });
       console.log(api);
     })
